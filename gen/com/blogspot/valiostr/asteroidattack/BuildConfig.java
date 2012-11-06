38a5b6593c0d0585ff73997e760982bceedd86ec
Lazutski <camicadze.pyshok@mail.ru>
2012-11-07 00:14:39 +0400

    start commit

---
+++ gen/com/blogspot/valiostr/asteroidattack/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.blogspot.valiostr.asteroidattack;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
